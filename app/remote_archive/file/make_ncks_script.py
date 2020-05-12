32c68e5f285ae4260e766f291ffd75c38c2cfc1e
Modellers Operational <modop@phorcys.npm.ac.uk>
2020-05-12 16:25:49 +0100

    Remote archive time tweak

--- app/remote_archive/file/make_ncks_script.py
+++ app/remote_archive/file/make_ncks_script.py
@@ -18,7 +18,7 @@ this_filestr = '{}/{}_0001.nc'.format(fvcom_output_path,fvcom_grid_name)
 this_fr = pf.read.FileReader(this_filestr)
 
 for i, this_date in enumerate(date_list):
-    time_ind = [np.argwhere(this_fr.time.datetime == this_date), np.argwhere(this_fr.time.datetime == this_date + dt.timedelta(hours=23))]
+    time_ind = [np.argwhere(this_fr.time.datetime == this_date), np.argwhere(this_fr.time.datetime == this_date + dt.timedelta(hours=24))]
 
     this_out_filestr = '{}_{}.nc'.format(fvcom_grid_name, this_date.strftime('%Y-%m-%d'))
     with open('make_daily_nc.sh', 'a') as f:
